e83338ca1049e61c9ef258f10fca32b4296e4531
Ken Johnson <k.rjohnson94@outlook.com>
2023-04-12 07:39:31 -0400

    Wired up doors for the most part

--- Assets/Scripts/DialogueManager.cs
+++ Assets/Scripts/DialogueManager.cs
@@ -9,6 +9,7 @@ public class DialogueManager : MonoBehaviour
 	public TextMeshProUGUI DialogueText;
 	//public TextMeshProUGUI ContinueText;
 	public GameObject DialogueBox;
+	public UnityEngine.UI.Button button;
 
 	private Queue<string> sentences;
 	public bool DialogueStarted;
@@ -18,11 +19,13 @@ public class DialogueManager : MonoBehaviour
 		sentences = new();
 		//ContinueText.enabled = false;
 		DialogueStarted = false;
+		button.enabled = false;
 	}
 
 	public void StartDialogue(Dialogue d)
 	{
 		DialogueBox.SetActive(true);
+		button.enabled = true;
 		NameText.text = d.Name;
 
 		sentences.Clear();
@@ -47,6 +50,7 @@ public class DialogueManager : MonoBehaviour
 
 	public void EndDialogue() 
 	{
+		button.enabled = false;
 		DialogueBox.SetActive(false);
 		NameText.text = "";
 		DialogueStarted = false;
---
+++ Assets/Scripts/Door.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    public bool DoorOpened;
+    public bool DoorEnabled;
+
+    [SerializeField]
+    private SpriteRenderer DoorFrame;
+    [SerializeField]
+    private GameObject DoorOpen;
+	[SerializeField]
+	private GameObject DoorClosed;
+
+    [SerializeField]
+    private Sprite WallSprite;
+
+	// Start is called before the first frame update
+	void Start()
+    {
+        if (!DoorEnabled)
+        {
+            DoorFrame.sprite = WallSprite;
+            DoorClosed.SetActive(true);
+            DoorClosed.GetComponent<SpriteRenderer>().enabled = false;
+            DoorOpen.SetActive(false);
+        }
+        else
+        {
+			DoorOpen.SetActive(DoorOpened);
+			DoorClosed.SetActive(!DoorOpened);
+		}
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (DoorEnabled)
+        {
+			DoorOpen.SetActive(DoorOpened);
+			DoorClosed.SetActive(!DoorOpened);
+		}
+		
+	}
+}
